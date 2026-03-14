b0897b044766947adb0fd01faa520a332326dae6
KamiChips <chimomela.santos@gmail.com>
2026-03-14 16:51:21 -0700

    Barbie Home screen

---
+++ components/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  barbiePink: "#E0218A",
+  barbieLightPink: "#FFC1E3",
+  barbieSoftPink: "#FFE4F2",
+  barbieDarkPink: "#C2186A",
+  white: "#FFFFFF",
+  textDark: "#2A2A2A",
+  gray: "#6B7280",
+};
